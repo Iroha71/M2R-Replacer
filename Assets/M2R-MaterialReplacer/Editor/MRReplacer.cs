9e3303284789e8d72a563d32e2f311667dc77017
Iroha71 <y.koba.h1071@outlook.jp>
2021-12-30 11:11:34 +0900

    [update] BuiltInシェーダーに対応

--- Assets/M2R-MaterialReplacer/Editor/MRReplacer.cs
+++ Assets/M2R-MaterialReplacer/Editor/MRReplacer.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEngine.Rendering;
 
 public class MRReplacer : EditorWindow
 {
@@ -9,7 +10,9 @@ public class MRReplacer : EditorWindow
     private const float M_OPAQUE = 0f, M_CUTOUT = 1f, M_TRANSPARENT = 2f, M_TRANSPARENT_Z = 3f;
     private const float CULL_OFF = 0f, CULL_FRONT = 1f, CULL_BACK = 2f;
     private const string REALTOON_SHADER_PATH_URP = "Universal Render Pipeline/RealToon/Version 5/Default/Default";
+    private const string REALTOON_SHADER_PATH_BUILTIN = "RealToon/Version 5/Default/";
     private Color outlineColor;
+    private enum RenderPipeline { BuiltIn, URP, HDRP };
     [MenuItem("Tools/M2R-MaterialReplacer")]
     private static void Create() {
         CreateWindow<MRReplacer>("M2R-MaterialReplacer");
@@ -26,9 +29,35 @@ public class MRReplacer : EditorWindow
         }
     }
 
+    private void ReplaceRealToonBuiltIn(List<Material> vrmMaterials) {
+        foreach (Material vrmMaterial in vrmMaterials) {
+            float renderMode = vrmMaterial.GetFloat("_BlendMode");
+            float cullMode = vrmMaterial.GetFloat("_CullMode");
+            Color litColor = vrmMaterial.GetColor("_Color");
+            Color shadeColor = vrmMaterial.GetColor("_ShadeColor");
+            int renderQueue = vrmMaterial.renderQueue;
+            string shaderType = renderMode == M_TRANSPARENT || renderMode == M_TRANSPARENT_Z ? "Fade Transparency" : "Default";
+            vrmMaterial.shader = Shader.Find(REALTOON_SHADER_PATH_BUILTIN + shaderType);
+            if (renderMode == M_CUTOUT) {
+                vrmMaterial.SetFloat("_N_F_CO", 1f);
+                vrmMaterial.SetFloat("_Cutout", 0.5f);
+            }
+            vrmMaterial.SetColor("_OutlineColor", outlineColor);
+            vrmMaterial.SetFloat("_Culling", cullMode);
+            vrmMaterial.renderQueue = renderQueue;
+            vrmMaterial.SetColor("_MainColor", litColor);
+            vrmMaterial.SetColor("_OverallShadowColor", shadeColor);
+        }
+    }
+
     private void ReplaceRealToon(List<Material> vrmMaterials) {
         if (vrmMaterialFolder == null || vrmMaterials.Count <= 0)
             return;
+
+        if (!GraphicsSettings.renderPipelineAsset) {
+            ReplaceRealToonBuiltIn(vrmMaterials);
+            return;
+        }
 
         foreach (Material vrmMaterial in vrmMaterials) {
             float renderMode = vrmMaterial.GetFloat("_BlendMode");
